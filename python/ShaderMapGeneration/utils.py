feb4449a1086a6959f976deeed4e3ccc513a93e6
Selim Benanni <s.bennani@wewanttoknow.com>
2023-05-22 10:42:04 +0700

    Clean

--- python/ShaderMapGeneration/utils.py
+++ python/ShaderMapGeneration/utils.py
@@ -6,8 +6,12 @@ def show(img):
     pilImage = Image.fromarray(img)
     display(pilImage)
 
+q = 10 # quantization
+offset = 5
+range = 255 - q - offset
+
 def getColor(i):
-    r = 240 - int(i / 16)*15
-    g = 240 - (i*15)%240
-    b = 0
+    r = range - int(i / (range/q))*q
+    g = range - (i*q)%range
+    b = q
     return (r,g,b)
--- python/test_matplotlib.py
+++ python/test_matplotlib.py
@@ -3,14 +3,10 @@ import cv2
 from matplotlib import pyplot as plt
 
 imgPath = os.path.join('textures', 'goomba.png')
-# Load the image
 img = cv2.imread(imgPath)
 
-# Convert the image from BGR to RGB
 img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-# Use plt.imshow() to add the image to the matplotlib plot
 plt.imshow(img_rgb)
 
-# Display the plot with the image
 plt.show()
